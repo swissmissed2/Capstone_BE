c4a97f9b0049bab7c09f01d9fb0c231b87d2a121
swissmissed <shkim7020@naver.com>
2025-03-31 22:09:54 +0900

    init commit

---
+++ src/main/java/com/capstonebe/capstonebe/CapstoneBeApplication.java
@@ -0,0 +1,13 @@
+package com.capstonebe.capstonebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstonebe/capstonebe/CapstoneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstonebe.capstonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
